37eded1bf5740a40981b9ae0f734eface8f74d2f
FaithSlade <faithwslade@gmail.com>
2021-04-24 13:53:47 -0700

    added one more line

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 
         System.out.println("Hello World");
         System.out.println("YAY STUFFS ARE ON GITHUB");
+        System.out.println("Adding GitHub now");
     }
 }
